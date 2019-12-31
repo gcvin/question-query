52f340a711569aeab393398b391c044bea19a9cc
张林 <zhanglin@cmiot.chinamobile.com>
2019-12-31 18:28:33 +0800

    feat: 增加云函数

--- cloud/functions/excel/index.js
+++ cloud/functions/excel/index.js
@@ -4,31 +4,41 @@ const xlsx = require('node-xlsx')
 cloud.init()
 
 const db = cloud.database()
+const categoryDB = db.collection('category')
 
 exports.main = async(event) => {
   const { fileID } = event
   const res = await cloud.downloadFile({ fileID })
-  const buffer = res.fileContent
-  const sheet = xlsx.parse(buffer)[0]
+  const sheets = xlsx.parse(res.fileContent)
+  const rows = sheets[0].data
 
-  const category = []
+  const categoryNames = []
+  const categoryIds = []
+  const question = rows.map(async row => {
+    const [category_name, question, answer, time, remark] = row
+    const index = categoryNames.indexOf(category_name)
+    if (index === -1) {
+      categoryNames.push(category_name)
+    }
+    return {
+      category_name,
+      question,
+      answer,
+      time,
+      remark
+    }
+  })
+
+  const tasks = category.map(item => categoryDB.add({
+    data: {
+      name: item
+    }
+  }))
+
+
   const question = []
 
-  for (let rowId in sheet['data']) {
-    var row = sheet['data'][rowId]
-    if (rowId > 0 && row) {
-      const promise = db.collection('users')
-        .add({
-          data: {
-            name: row[0], //姓名
-            age: row[1], //年龄
-            address: row[2], //地址
-            wechat: row[3] //wechat
-          }
-        })
-      tasks.push(promise)
-    }
-  }
+  rows.map(row => row[0])
 
   let result = await Promise.all(tasks).then(res => {
     return res
